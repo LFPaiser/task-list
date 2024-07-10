6bf91ae3c7060f0414384809ed4a2d628fbe2a16
Luis Felipe <60530785+LFPaiser@users.noreply.github.com>
2024-07-10 18:39:13 -0300

    Base structure

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <div className="flex items-center justify-between">
+      <Link href="/" className="text-6xl font-extralight">
+        taskery
+      </Link>
+      <Link href="/" className="flex items-center gap-2 hover:underline">
+        <span className="text-lg">Conta</span>
+        <Image
+          src="https://github.com/LFPaiser.png"
+          className="h-10 w-10 rounded-full"
+          width={96}
+          height={96}
+          alt=""
+        />
+      </Link>
+    </div>
+  )
 }
